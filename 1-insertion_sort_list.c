b73558d2e8fc7b369461f64efeeacb3747d4060d
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-05-01 03:23:26 +0200

    solution for insertion.

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,11 +9,11 @@
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
 	(*n1)->next = n2->next;
-	if (n2->next != null)
+	if (n2->next != NULL)
 		n2->next->prev = *n1;
 	n2->prev = (*n1)->prev;
 	n2->next = *n1;
-	if ((*n1)->prev != null)
+	if ((*n1)->prev != NULL)
 		(*n1)->prev->next = n2;
 	else
 		*h = n2;
@@ -32,14 +32,14 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *iter, *insert, *tmp;
 
-	if (list == null || *list == null || (*list)->next == null)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (iter = (*list)->next; iter != null; iter = tmp)
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
 		tmp = iter->next;
 		insert = iter->prev;
-		while (insert != null && iter->n < insert->n)
+		while (insert != NULL && iter->n < insert->n)
 		{
 			swap_nodes(list, &insert, iter);
 			print_list((const listint_t *)*list);

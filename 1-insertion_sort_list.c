cfa7adb37df0212096348913ba7c7542e92999d1
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-05-01 03:17:18 +0200

    insertion sort.

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,38 +1,48 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - a function sorts a doubly linked list with insertion sort algo
- * @list: The linked list
- * Return: Nothing
+ * swap_nodes - swap two nodes in a listint_t doubly-linked list.
+ * @h: a pointer to the head of the doubly-linked list.
+ * @n1: a pointer to the first node to swap.
+ * @n2: the second node to swap.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != null)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != null)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: a pointer to the head of a doubly-linked list of integers.
+ *
+ * description: prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t **current = NULL;
-	listint_t **current2 = NULL;
-	listint_t **smallest = NULL;
+	listint_t *iter, *insert, *tmp;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == null || *list == null || (*list)->next == null)
 		return;
-	current = list;
-	while ((*current)->next != NULL)
+
+	for (iter = (*list)->next; iter != null; iter = tmp)
 	{
-		current2 = current;
-		while((*current2)->next != NULL)
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != null && iter->n < insert->n)
 		{
-			if ((*current2)->n < (*current)->n)
-				smallest = current2;
-			*current2 = (*current2)->next;
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
-		if (smallest != NULL)
-		{
-			/* TODO: Switch between the smallest and the current nodes*/
-			(*current)->prev = smallest->prev;
-			if ((*smallest)->prev != NULL)
-				(*smallest)->prev->next = (*smallest)->next;
-			(*smallest)->next = malloc(sizeof(listint_t));
-			(*smallest)->next = *current;
-		}
-		print_list(*list);
-		*current = (*current)->next;
 	}
 }
